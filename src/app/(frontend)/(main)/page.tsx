a3a57a865543a43c00807a7e65393ab736fcff7a
Long Tran Quang <longtrq26@gmail.com>
2026-02-02 08:12:09 +0700

    chore(landing): compose main page sections

--- src/app/(frontend)/(main)/page.tsx
+++ src/app/(frontend)/(main)/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import FeaturesSection from './_sections/features'
 import HeroSection from './_sections/hero'
+import HighlightsSection from './_sections/highlights'
 
 const MainPage = () => {
   return (
     <div>
       <HeroSection />
+      <HighlightsSection />
       <FeaturesSection />
     </div>
   )
