ab1f0996135812509bed7651818f292b05fb0576
Long Tran Quang <longtrq26@gmail.com>
2026-02-02 08:11:45 +0700

    feat(icons): add icons for landing highlights section

---
+++ src/components/icons/ic-research.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ICResearch = ({ className }: { className?: string }) => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 58 50' className={className}>
+      <path fill='#FFD11A' d='M13.413 34.542V50H44.36V34.542H13.413Z' />
+      <path
+        fill='#404040'
+        d='M44.353 34.53 57.74 26.8 42.266 0 28.88 7.729l15.473 26.8ZM28.86 7.75 15.474.02 0 26.821l13.386 7.73L28.86 7.75Z'
+      />
+    </svg>
+  )
+}
+
+export default ICResearch
